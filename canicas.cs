ac4e9f5dae452de4c43b90d2b0d6baf209fba32a
kchafla <kchaflam@fp.insjoaquimmir.cat>
2020-10-23 19:49:29 +0200

    Menu de canicas

--- canicas.cs
+++ canicas.cs
@@ -1,7 +1,32 @@
 using System;
 
 class MainClass {
+    public static void Menu () {
+        Console.WriteLine("Escribe tu nombre:");
+        string nombre = Console.ReadLine();
+
+        int posicion = 0;
+        int encontrado = 0;
+        for (int n = 0; n < nombres.length; n++)
+        {
+            if (String.Equals(nombre, nombres[n]))
+            {
+                posicion = n;
+                encontrado = 1; 
+            }
+        }
+
+        if (encontrado == 0)
+        {
+            Console.WriteLine("No se ha podido encontrar un niño con ese nombre!");
+        } else
+        {
+            Console.WriteLine("Se ha encontrado!")
+        }
+    }
+
     public static void Main () {
         Console.WriteLine("hola");
+
     }
 }
