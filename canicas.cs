145c2460ed940f4a7414f1eae1b178963fe6db74
alex2001-daw <abalaguer@fp.insjoaquimmir.cat>
2020-10-23 19:51:08 +0200

    listas

--- canicas.cs
+++ canicas.cs
@@ -1,7 +1,10 @@
 using System;
 
 class MainClass {
+    string [] nens = {"Joan", "Nil", "Roc", "Thiago", "Nico","Noa", "Gerard", "Nor"};
+    int [] canicas = {12,3,25,31,6,11,21,9};
+    string [] ciutat = {"Vilanova", "Canyelles", "Vilanova", "Sitges", "Cunit", "Vilanova", "Kazahstan", "Cubelles"};
     public static void Main () {
-        Console.WriteLine("hola");
+        
     }
 }
