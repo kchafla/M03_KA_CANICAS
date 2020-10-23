7858c3cd015f6e6a9824d47ad11fec03be11da24
kchafla <kchaflam@fp.insjoaquimmir.cat>
2020-10-23 19:55:43 +0200

    Canicas con arrays

--- canicas.cs
+++ canicas.cs
@@ -1,22 +1,16 @@
 using System;
 
 class MainClass {
-<<<<<<< HEAD
-    string [] nens = {"Joan", "Nil", "Roc", "Thiago", "Nico","Noa", "Gerard", "Nor"};
-    int [] canicas = {12,3,25,31,6,11,21,9};
-    string [] ciutat = {"Vilanova", "Canyelles", "Vilanova", "Sitges", "Cunit", "Vilanova", "Kazahstan", "Cubelles"};
-    public static void Main () {
         
-=======
-    public static void Menu () {
+    public static void Menu (string[] nens, int[] canicas, string[] ciutat) {
         Console.WriteLine("Escribe tu nombre:");
         string nombre = Console.ReadLine();
 
         int posicion = 0;
         int encontrado = 0;
-        for (int n = 0; n < nombres.length; n++)
+        for (int n = 0; n < nens.length; n++)
         {
-            if (String.Equals(nombre, nombres[n]))
+            if (String.Equals(nombre, nens[n]))
             {
                 posicion = n;
                 encontrado = 1; 
@@ -33,8 +27,9 @@ class MainClass {
     }
 
     public static void Main () {
-        Console.WriteLine("hola");
-
->>>>>>> dce1961b033b14c851df6c1747077a1cb0024c33
+        string[] nens = {"Joan", "Nil", "Roc", "Thiago", "Nico","Noa", "Gerard", "Nor"};
+        int[] canicas = {12,3,25,31,6,11,21,9};
+        string[] ciutat = {"Vilanova", "Canyelles", "Vilanova", "Sitges", "Cunit", "Vilanova", "Kazahstan", "Cubelles"};
+        Menu(nens, canicas, ciutat);
     }
 }
